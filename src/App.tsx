6a11a8dc64a8efd64550c200eef64985419fa6ec
Thanachit Sengsalee <6510120049@email.psu.ac.th>
2023-07-23 15:58:15 +0700

    add component all

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
 import logo from "./logo.svg";
 
-import "./App.css";
-
 function App() {
   return (
     <div className="font-inter">
-      <div className="app-header">
-        <img src={logo} alt="logo" /> <span>Drivehub</span>
+      {/* NAVBAR */}
+      <div className="shadow-lg">
+        <div className="mx-20 flex justify-between h-20">
+          <div className="flex">
+            <img src={logo} alt="logo" />
+            <span className="my-auto font-bold text-xl">Drivehub</span>
+          </div>
+          <div className="flex">
+            {/* <img src={logo} alt="logo" /> */}
+            <span className="my-auto">Cart (0)</span>
+          </div>
+        </div>
       </div>
-      <div className="flex">
-        <div>Car list</div>
-        <div>Cart</div>
+
+      {/* FILTER DIV */}
+      <div className="flex justify-between  mx-20 my-8">
+        <div className="text-2xl font-bold">Car Available</div>
+        <div className="flex">
+          <div>Search Car</div>
+          <div>Sort Price</div>
+        </div>
       </div>
-      <div className="app-footer">FOOTER</div>
+
+      {/* LISTS CARS */}
+      <div className="flex min-h-screen   bg-gray-100">
+        <div className="mx-20 w-full">
+          <div className="w-full">list car</div>
+        </div>
+      </div>
+
+      {/* FOOTER */}
+      <div className="">FOOTER</div>
     </div>
   );
 }
